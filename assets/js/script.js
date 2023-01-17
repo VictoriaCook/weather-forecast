5db50ae209468ad9f532e3b2bed3f2f8e66b1443
VC <victoriarccook@gmail.com>
2023-01-17 12:51:36 +1100

    added consts to get elements by id

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,17 @@
-const grabBtn = document.getElementById("getCity");
+const getBtn = document.getElementById("getCity");
 var cityName = "Melbourne"
 const APIKEY = "825021d5522a53177b5a5f2b72cf874e"
 
-grabBtn.addEventListener("click", getCoordinates);
+// get current weather div ids to populate with weather data
+
+const getCurrentContainer = document.getElementById("currentContainer");
+const getCityDisplay = document.getElementById("cityDisplay");
+const getCurrentWeatherImg = document.getElementById("currentWeatherImg");
+const getTempDisplay = document.getElementById("tempDisplay");
+const getWindDisplay = document.getElementById("windDisplay");
+const getHumidityDisplay = document.getElementById("humidityDisplay");
+
+getBtn.addEventListener("click", getCoordinates);
 
 // async function getCoordinates() {
 //     let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q${cityName}&appid=${APIKEY}`
