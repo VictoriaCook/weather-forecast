c32d26d894e2ab985b41c7d77099ad9e361ac0d1
VC <victoriarccook@gmail.com>
2023-01-17 14:14:46 +1100

    setting up more consts for getting elements by id

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const getBtn = document.getElementById("getCity");
 var cityName = "Melbourne"
 const APIKEY = "825021d5522a53177b5a5f2b72cf874e"
 
-// get current weather div ids to populate with weather data
+// Get current weather div ids to populate with weather data
 
 const getCurrentContainer = document.getElementById("currentContainer");
 const getCityDisplay = document.getElementById("cityDisplay");
@@ -11,6 +11,35 @@ const getTempDisplay = document.getElementById("tempDisplay");
 const getWindDisplay = document.getElementById("windDisplay");
 const getHumidityDisplay = document.getElementById("humidityDisplay");
 
+// Get day 1 forecast ids to populate with weather data
+
+const  getDay1Container= document.getElementById("day1");
+const  get1Date = document.getElementById("1date");
+const  get1WeatherImg = document.getElementById("1weatherImg");
+const  get1TempDisplay = document.getElementById("1tempDisplay");
+const  get1WindDisplay = document.getElementById("1windDisplay");
+const  get1HumidityDisplay = document.getElementById("1humidityDisplay");
+
+// Get day 2 forecast ids to populate with weather data
+
+const  getDay2Container= document.getElementById("day2");
+const  get2Date = document.getElementById("2date");
+const  get2WeatherImg = document.getElementById("2weatherImg");
+const  get2TempDisplay = document.getElementById("2tempDisplay");
+const  get2WindDisplay = document.getElementById("2windDisplay");
+const  get2HumidityDisplay = document.getElementById("2humidityDisplay");
+
+// Get day 3 forecast ids to populate with weather data
+
+const  = document.getElementById("");
+const  = document.getElementById("");
+const  = document.getElementById("");
+const  = document.getElementById("");
+const  = document.getElementById("");
+const  = document.getElementById("");
+
+// Add event listener to search button
+
 getBtn.addEventListener("click", getCoordinates);
 
 // async function getCoordinates() {
@@ -18,6 +47,8 @@ getBtn.addEventListener("click", getCoordinates);
 //     let result = await fetch(apiUrl)
 //     console.log(result);
 // }
+
+// API call URL: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 function getCoordinates() {
     let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q${cityName}&appid=${APIKEY}`
@@ -34,4 +65,11 @@ function getCoordinates() {
 
 }
 
-// async await: waiting for fetch to do its thing before we print the result
+// Could've created the divs with a for loop to create and populate
+// for (let i = 0; i < 4; i++) {
+//     createButton = document.createElement('button');
+//     createButton.id = 'buttonId'+i;
+//     createButton.classList.add('mc-buttons');
+//     createButton.addEventListener('click', showQuestions); 
+//     quiz.appendChild(createButton);
+// }
